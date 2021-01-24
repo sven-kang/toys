bfef06e348a05aea4b19d6b8655a9ec6021fb54e
Sven Kang <sven.c.kang@gmail.com>
2021-01-24 07:46:36 -0600

    feat: adding hello world fastify app

---
+++ fastify/server.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { hello: 'world' }
+});
+
+(async () => {
+  try {
+    await fastify.listen(3000)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})();
