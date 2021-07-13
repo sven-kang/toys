7f01d5a57c097cd155a2a2322dab721554774960
Sven Kang <sven.c.kang@gmail.com>
2021-07-12 21:55:42 -0500

    update

---
+++ todo-recoil/src/components/ToDoCard/ToDoCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ToDoCard.css';
+
+function ToDoCard() {
+  return (
+    <div className="ToDoCard">
+      Hello World!
+    </div>
+  );
+}
+
+export default ToDoCard;
